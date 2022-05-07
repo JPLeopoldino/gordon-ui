989240c820a2f53c76f0552a5c9cc28c018b97f6
Joao Pedro <leopoldino26@gmail.com>
2022-05-07 17:50:35 -0300

    feat: ensure Input component exports

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import { NativeModules } from 'react-native'
 
 import Card from './components/Card'
 import { Counter } from './components/couter'
+import Input from './components/Input'
+import InputMask from './components/Input/InputMask'
 
-export { Card, Counter }
+export { Card, Counter, Input, InputMask }
 
 export default NativeModules.GordonUIModule
