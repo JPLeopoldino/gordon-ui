3dbceb9fad7846190ca240937dbc09ae4e42606a
Joao Pedro <leopoldino26@gmail.com>
2022-05-16 00:28:27 -0300

    feat: ensure Button component exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { NativeModules } from 'react-native'
 
+import Button from './components/Button'
 import Card from './components/Card'
 import { Counter } from './components/couter'
 import Input from './components/Input'
 import InputMask from './components/Input/InputMask'
 
-export { Card, Counter, Input, InputMask }
+export { Button, Card, Counter, Input, InputMask }
 
 export default NativeModules.GordonUIModule
